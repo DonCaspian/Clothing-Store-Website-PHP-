9a0dadcdde70b47c4ffc57d5591544cd7a78a438
Jai <169172219+DonCaspian@users.noreply.github.com>
2026-07-14 09:28:46 +0000

    new file:   customer/change_pass.php 	new file:   customer/confirm.php 	new file:   customer/customer_images/sample_image.jpg 	new file:   customer/customer_images/user-icn-min.png 	new file:   customer/customer_images/user.jpg 	new file:   customer/customer_login.php 	new file:   customer/delete_account.php 	new file:   customer/delete_wishlist.php 	new file:   customer/edit_account.php 	new file:   customer/font-awesome/css/font-awesome.css 	new file:   customer/font-awesome/css/font-awesome.min.css 	new file:   customer/font-awesome/font/FontAwesome.otf 	new file:   customer/font-awesome/font/fontawesome-webfont.eot 	new file:   customer/font-awesome/font/fontawesome-webfont.svg 	new file:   customer/font-awesome/font/fontawesome-webfont.ttf 	new file:   customer/font-awesome/font/fontawesome-webfont.woff 	new file:   customer/fonts/fontello.eot 	new file:   customer/fonts/fontello.svg 	new file:   customer/fonts/fontello.ttf 	new file:   customer/fonts/fontello.woff 	new file:   customer/fonts/fontello.woff2 	new file:   customer/fonts/glyphicons-halflings-regular.eot 	new file:   customer/fonts/glyphicons-halflings-regular.svg 	new file:   customer/fonts/glyphicons-halflings-regular.ttf 	new file:   customer/fonts/glyphicons-halflings-regular.woff 	new file:   customer/fonts/glyphicons-halflings-regular.woff2 	new file:   customer/fonts/montserrat-light-webfont.svg 	new file:   customer/fonts/montserrat-light-webfont.ttf 	new file:   customer/fonts/montserrat-light-webfont.woff 	new file:   customer/fonts/montserrat-light-webfont.woff2 	new file:   customer/functions/functions.php 	new file:   customer/images/banner-ave.jpg 	new file:   customer/images/logo.png 	new file:   customer/includes/db.php 	new file:   customer/includes/footer.php 	new file:   customer/includes/header.php 	new file:   customer/includes/main.php 	new file:   customer/includes/sidebar.php 	new file:   customer/js/bootstrap.min.js 	new file:   customer/js/jquery.min.js 	new file:   customer/logout.php 	new file:   customer/my_account.php 	new file:   customer/my_orders.php 	new file:   customer/my_wishlist.php 	new file:   customer/pay_offline.php 	new file:   customer/styles/backend.css 	new file:   customer/styles/bootstrap.min.css

---
+++ customer/pay_offline.php
@@ -0,0 +1,50 @@
+<center>
+
+<h1> Pay OffLine Using Method  </h1>
+
+<p class="text-muted" >
+
+If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+
+</p>
+
+</center>
+
+<hr>
+
+<div class="table-responsive" >
+
+<table class="table table-bordered table-hover table-striped" >
+
+<thead>
+
+<tr>
+
+<th> Bank Account Details </th>
+
+<th> UBL Omni,Mobi Cash Details: </th>
+
+<th> Western Union Details: </th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+<tr>
+
+<td> Bank Name:ubl Account No:03333333 Branch Code:0342 Branch Name:DemoBranch	 </td>
+
+<td> NIC#001230006 Mobile No:7410000000, Name:DemoName </td>
+
+<td> Full Name:Demo Name, Mobile No:7000015000, Name:Demo, Country:US, N.I.C No:011234567
+</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+</div>
